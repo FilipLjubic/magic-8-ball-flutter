cc40457720d7a5feef086b15c5d604b6450219af
Filip Ljubic <f.ljubic7@hotmail.com>
2020-07-27 13:15:58 +0200

    gotovo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        title: "Ask Me Anything",
+        home: Home(),
       ),
     );
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int answer = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[700],
+        title: Text(
+          "Ask Me Anything",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: FlatButton(
+          child: Image.asset('images/ball$answer.png'),
+          onPressed: () => setState(() => answer = Random().nextInt(5) + 1),
+        ),
+      ),
+    );
+  }
+}
